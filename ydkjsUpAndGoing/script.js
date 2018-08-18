ab4e6888d5f6cfa676f77b6b5194a8d68eccfc15
Kenneth Solano <klsolano77@gmail.com>
2018-08-18 14:30:02 -0800

    got a little taste of YDKJS

--- ydkjsUpAndGoing/script.js
+++ ydkjsUpAndGoing/script.js
@@ -3,8 +3,18 @@ a = 25;
 b = a * 2;
 console.log(b,"this is the result");
 
+//Menu
+food = prompt("what do you want to eat");
+console.log(food);
 
-
+//Personal data (object)
+var kenneth = { 
+  name: 'Kenneth',
+age: 21,
+food: 'pizza',
+dog: 'husky',
+}
+console.log(kenneth)
 
 
 
